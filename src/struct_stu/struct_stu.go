1154b16414bbe328020e2972d6e66b5c4cb9e6af
王晶05 <wangjing05@qutoutiao.net>
2020-02-27 00:19:57 +0800

    add demo

--- src/struct_stu/struct_stu.go
+++ src/struct_stu/struct_stu.go
@@ -8,42 +8,125 @@ import "fmt"
 type student struct {
 	id    int
 	name  string
-	age   int
-	score float32
-}
-type class struct {
-	Map map[int]*student
+	class string
 }
 
-func (c *class) addStu() {
-	var id int
-	var name string
-	var age int
-	var score float32
-	fmt.Println("输入id，必须为整数：")
-	_, err := fmt.Scan(&id)
-	fmt.Println("输入name：")
-	_, err = fmt.Scan(&name)
-	fmt.Println("输入age，必须为整数：")
-	_, err = fmt.Scan(&age)
-	fmt.Println("输入score，浮点数：")
-	_, err = fmt.Scan(&score)
-	if err != nil {
-		fmt.Println("出错")
-	}
-	_, isSave := c.Map[id]
-	if isSave {
-		fmt.Println("id已存在，请更换")
-		return
-	}
-	stu := &student{
+func newStudent(id int, name, class string) *student {
+	return &student{
 		id:    id,
 		name:  name,
-		age:   age,
-		score: score,
+		class: class,
 	}
-	c.Map[id] = stu
-	fmt.Println("保存成功")
 }
-func (s *student) listStu() {}
-func main()                 {}
+
+type studentMgr struct {
+	allStudents []*student
+}
+
+func newStudentMgr() *studentMgr {
+	return &studentMgr{
+		allStudents: make([]*student, 0, 100),
+	}
+}
+
+//1. add
+func addStudent(s *studentMgr, newStu *student) {
+	s.allStudents = append(s.allStudents, newStu)
+}
+
+//2. modify
+func modifyStudent(s *studentMgr, newStu *student) {
+	for i, v := range s.allStudents {
+		if newStu.id == v.id {
+			s.allStudents[i] = newStu
+			return
+		}
+	}
+	//
+	fmt.Println("在系统中没有找到对应的学号！")
+}
+
+//3. show
+func showStudent(s *studentMgr) {
+	for _, v := range s.allStudents {
+		fmt.Printf("学号：%d; 姓名 %s; 班级 %s\n", v.id, v.name, v.class)
+	}
+}
+
+//4. delete
+func delStudent(s *studentMgr, id int) {
+	for i, v := range s.allStudents {
+		if v.id == id {
+			fmt.Println("要删除的id，当前id，第几轮循环", id, i)
+			s.allStudents = append(s.allStudents[:i], s.allStudents[i+1:]...)
+			return
+		}
+	}
+	fmt.Println("在系统中没有找到对应的学号！")
+}
+
+func showMenu() {
+	fmt.Printf("请选择功能：\n")
+	fmt.Printf("1. 展示学生列表\n")
+	fmt.Printf("2. 添加学生\n")
+	fmt.Printf("3. 编辑学生信息\n")
+	fmt.Printf("4. 删除学生\n")
+}
+func getID() int {
+	var id int
+	fmt.Print("请输入学员的学号：")
+	fmt.Scanf("%d\n", &id)
+	return id
+}
+func getInput() *student {
+	var (
+		id    int
+		name  string
+		class string
+	)
+	fmt.Println("请按要求输入学员信息")
+	fmt.Print("请输入学员的学号：")
+	fmt.Scanf("%d\n", &id)
+	fmt.Print("请输入学员的姓名：")
+	fmt.Scanf("%s\n", &name)
+	fmt.Print("请输入学员的班级：")
+	fmt.Scanf("%s\n", &class)
+	stu := newStudent(id, name, class)
+	return stu
+}
+
+func main() {
+	sm := newStudentMgr()
+	for i := 0; i < 10; i++ {
+		tmpStu := newStudent(i, fmt.Sprintf("stu%02d", i), "萌萌哒")
+		sm.allStudents = append(sm.allStudents, tmpStu)
+	}
+	var choice int
+	for {
+		showMenu()
+		//输入
+		fmt.Println("请输入你要操作的序号")
+		fmt.Scanf("%d\n", &choice)
+		fmt.Println("你输入的是:", choice)
+		switch choice {
+		case 1:
+			//list
+			showStudent(sm)
+		case 2:
+			//add添加
+			stu := getInput()
+			addStudent(sm, stu)
+		case 3:
+			//edit
+			stu := getInput()
+			modifyStudent(sm, stu)
+		case 4:
+			//delete
+			id := getID()
+			delStudent(sm, id)
+
+		default:
+			fmt.Printf("请重新选择\n")
+		}
+	}
+}
